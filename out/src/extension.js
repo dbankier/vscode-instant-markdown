6d85e9ba18f8c3b8c88df88cd942967e52b9b7e6
David Bankier <dbankier@hotmail.com>
2020-09-02 10:52:52 +0300

    rebuild

--- out/src/extension.js
+++ out/src/extension.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
